31ee4ae5c90cf3f32c56b1c05e7c5afeaa1a6f02
Whitney Broadfoot <55456375+whitneybroadfoot@users.noreply.github.com>
2019-11-24 18:10:56 -0700

    Commit

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ $(document).ready(function(){
     $(".saveBtn").click(function(){
         var currentButton = $(this).val()
         var input = $(currentButton).val()
-        
-        console.log(input);
+console.log(input);
     })
 })
 
+document.getElementById("saveBtn").addEventListener("click", function(){
+    var input = {
+        userMeeting: "9am".value()
+    };
+
+    localStorage.setItem("userMeeting", value);
+console.log(value);
+});
+
+
+
+// use .past .present .future for moment.js
