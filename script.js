a8edbe4eb67bd7369f7388eacb4af13271d8d251
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-03 12:51:15 -0700

    clean code

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-    //script for current day
+//script for current day
     "date"
     var thisDay = moment();
     $("#currentDay").append(thisDay.format('LL'));
-    //current, past, present determining colors
+//current, past, present determining colors
     function colorFunction() {
         var now = parseInt(moment().format("H"))
         console.log(now);
         for (var i = 9; i < 18; i++) {
             var id = "#" + i;
-            //determining what colors
+//determining what colors
             if (i < now) {
                 $(id).removeClass()
                 $(id).attr("class", "past")
