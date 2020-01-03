084f3b9461f6fc305c16eb2848286749599e9768
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-03 11:11:30 -0700

    save to local

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 $(document).ready(function () {
-//script for current day
+    //script for current day
     "date"
     var thisDay = moment();
     $("#currentDay").append(thisDay.format('LL'));
-//current, past, present determining colors
+    //current, past, present determining colors
     function colorFunction() {
         var now = parseInt(moment().format("H"))
         console.log(now);
         for (var i = 9; i < 18; i++) {
             var id = "#" + i;
-//determining what colors
+            //determining what colors
             if (i < now) {
                 $(id).removeClass()
                 $(id).attr("class", "past")
@@ -22,5 +22,21 @@ $(document).ready(function () {
             }
         }
     }
+//running continually with 1000
+    setInterval(colorFunction, 1000)
+    $(".saveBtn").click(function () {
+        var currentButton = $(this).val()
+        var input = $(currentButton).val()
+        console.log(input);
+        localStorage.setItem(currentButton, input);
+    })
+})
+//getting text to stay after page refreshes
+var times = ["#9am", "#10am", "#11am", "#12pm", "#1pm", "#2pm", "#3pm", "#4pm", "#5pm"]
 
+for (var i = 0; i < times.length; i++) {
+    var id = times[i]
+    var info = localStorage.getItem(id)
+    $(id).val(info)
+}
 
