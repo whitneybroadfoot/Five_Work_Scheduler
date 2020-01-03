466ba9913434c266ce1bd9de0864e4a04da03633
Whitney-Broadfoot <whitneybroadfoot@gmail.com>
2020-01-03 11:07:27 -0700

    colors for time

--- script.js
+++ script.js
@@ -1,24 +1,26 @@
-$(document).ready(function(){
-    $(".saveBtn").click(function(){
-        var currentButton = $(this).val()
-        var input = $(currentButton).val()
-console.log(input);
-    })
-})
+$(document).ready(function () {
+//script for current day
+    "date"
+    var thisDay = moment();
+    $("#currentDay").append(thisDay.format('LL'));
+//current, past, present determining colors
+    function colorFunction() {
+        var now = parseInt(moment().format("H"))
+        console.log(now);
+        for (var i = 9; i < 18; i++) {
+            var id = "#" + i;
+//determining what colors
+            if (i < now) {
+                $(id).removeClass()
+                $(id).attr("class", "past")
+            } else if (i === now) {
+                $(id).removeClass()
+                $(id).attr("class", "present")
+            } else if (i > now) {
+                $(id).removeClass()
+                $(id).attr("class", "future")
+            }
+        }
+    }
 
 
-
-
-// localStorage.setItem("saveBtn", "9am");
-
-
-// var input = ("#saveBtn").text(moment)
-//     console.log(moment);
-
-// use .past .present .future for moment.js
-
-// //  
-// $(document).ready(function() {
-//     const date = moment();
-//     console.log(moment());
-//     $("#currentDay").html(moment().format("LL"));
